54366492c8e9d467671311197987e6dc6e729140
Aryan <ayu.arya72@gmail.com>
2022-06-02 14:29:49 +0530

    Added edit product function

--- models/product.js
+++ models/product.js
@@ -13,28 +13,41 @@ const getProductsFromFile = (cb) => {
 
 }
 module.exports = class Product {
-    constructor(title,imageUrl,description,price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id
         this.title = title;
-        this.imageUrl=imageUrl
-        this.description=description
-        this.price=price;
+        this.imageUrl = imageUrl
+        this.description = description
+        this.price = +price;
     }
-    save()
-    {
-        this.id=Math.random().toString();
+    save() {
+        
         getProductsFromFile(products => {
-          products.push(this);
-          fs.writeFile(p, JSON.stringify(products), err => {
-            console.log(err,'here');
-          });
+            if (this.id) {
+                const existingProductIndex = products.findIndex((prod) => prod.id === this.id)
+                const updatedProduct = [...products];
+                updatedProduct[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+                    console.log(err, 'here');
+                });
+
+            }
+            else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    console.log(err, 'here');
+                });
+            }
+
         });
     }
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
-    static findById(id,cb){
-        getProductsFromFile(products=>{
-            const product=products.find(p=>p.id===id)
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id)
             cb(product)
         })
     }
